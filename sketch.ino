445d7ece80921a64479c682ad8a79343057633e2
rafaelrbcleao-lgtm <rafaelrbcleao@usp.br>
2026-04-24 09:52:35 -0300

    Update para semaforo (3 LEDS)

--- sketch.ino
+++ sketch.ino
@@ -1,40 +1,22 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(8, OUTPUT);
+  // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(12, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(8, LOW);
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(8, HIGH);
-  delay(1000);                       // wait for a second
+  // put your main code here, to run repeatedly:
+digitalWrite(12, HIGH);
+digitalWrite(13, LOW);
+digitalWrite(8, LOW);
+delay(5000);
+digitalWrite(12, LOW);
+digitalWrite(13, LOW);
+digitalWrite(8, HIGH);
+delay(1000);
+digitalWrite(12, LOW);
+digitalWrite(13, HIGH);
+digitalWrite(8, LOW);
+delay(5000);
 }
